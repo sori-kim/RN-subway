e7b5e4adf47fe7f3e73a20ac4a742b0880b6b7d6
rimuuu <soryonew@gmail.com>
2020-07-07 11:58:04 +0900

    [Add] Splited files to make modules - Created Drawer / Tab folders - Created IMAGE variable for handling  images sources

---
+++ src/components/shared/Image.tsx
@@ -0,0 +1,7 @@
+const IMAGE = {
+    ICON_MENU: require('../../../assets/hamburger.png'),
+    ICON_BACK: require('../../../assets/back.png'),
+    ICON_USER: require('../../../assets/user.png'),
+}
+
+export {IMAGE}
